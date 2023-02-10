8343b733db8bf4fd2d69fa6c2c4864a49f43288e
ezriharmusial <ezriharmusial@gmail.com>
2023-02-10 22:44:37 +0100

    No imageprocessors

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-// import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
-import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte(), tailwind(), astroImageTools],
+  integrations: [mdx(), sitemap(), svelte(), tailwind()],
   output: "server",
   adapter: netlify()
 });
