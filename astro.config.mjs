2795253d1ab10ab8f9b652a658958ba99e816d1c
ezriharmusial <ezriharmusial@gmail.com>
2023-02-10 23:56:43 +0100

    init critters, deadlinks, browsersync, pwa test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,47 @@ import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import { VitePWA } from 'vite-plugin-pwa';
+import VitePluginBrowserSync from 'vite-plugin-browser-sync';
+
+// https://astro.build/config
+import critters from "astro-critters";
+
+// https://astro.build/config
+import deadLinks from "astro-dead-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  integrations: [mdx(), sitemap(), svelte(), tailwind(), critters(), deadLinks(),],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+
+  vite: {
+    plugins: [VitePluginBrowserSync(), VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null,
+        runtimeCaching: [{
+          urlPattern: /^https:\/\/fonts\.cdnfonts\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'cdn-fonts-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+            },
+
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }]
+      }
+    })]
+  },
 });
