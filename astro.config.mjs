3ea8484640960e72b430c5b1a553407f10f23684
ezriharmusial <ezriharmusial@gmail.com>
2023-02-10 14:45:07 +0100

    GetEntryBySlug, Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
