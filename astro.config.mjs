01832d0d64bb324d63bbb5cc7383a4bbe09116e1
ezriharmusial <ezriharmusial@gmail.com>
2023-02-10 21:56:45 +0100

    Imagetools test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+// import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte(), tailwind(), image({serviceEntryPoint: '@astrojs/image/sharp'})],
+  integrations: [mdx(), sitemap(), svelte(), tailwind(), astroImageTools],
   output: "server",
   adapter: netlify()
 });
